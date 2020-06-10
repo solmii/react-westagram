f95d298f0621289af3d7a9d39c2834692e86ab19
solmii <rkdthfal4759@naver.com>
2020-06-10 15:34:28 +0900

    react : add component "FollowProfile"

---
+++ src/Components/follow_profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../Pages/Main/Main.css';
+
+class FollowProfile extends React.Component {
+  render() {
+    return (
+      <div className='follow_profile'>
+        <div className='title_img_radius'>
+          <img
+            src='https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=Bj3WvVLmBYAAX_EHiGC&oh=d81e5bbacfadeb11cf773cc7c6a3650d&oe=5F004B70'
+            alt='>WECODE'
+          />
+        </div>
+        <div className='follow_form'>
+          <a className='feed_title_main-head' href=''>
+            wecode_bootcamp
+          </a>
+          <a className='feed_title_sub-head sidebar_name' href=''>
+            회원님을 팔로우합니다
+          </a>
+        </div>
+        <div className='follow_btn'>
+          <button>팔로우</button>
+        </div>
+      </div>
+    );
+  }
+}
+export default FollowProfile;
--- src/Pages/Main/Aside.js
+++ src/Pages/Main/Aside.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FollowProfile from '../../Components/follow_profile';
 import './Main.css';
 
 class Aside extends React.Component {
@@ -28,105 +29,15 @@ class Aside extends React.Component {
           </div>
 
           <div className='follow'>
-            <div className='follow_profile'>
-              <div className='title_img_radius'>
-                <img
-                  src='https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=Bj3WvVLmBYAAX_EHiGC&oh=d81e5bbacfadeb11cf773cc7c6a3650d&oe=5F004B70'
-                  alt='>WECODE'
-                />
-              </div>
-              <div className='follow_form'>
-                <a className='feed_title_main-head' href=''>
-                  wecode_bootcamp
-                </a>
-                <a className='feed_title_sub-head sidebar_name' href=''>
-                  회원님을 팔로우합니다
-                </a>
-              </div>
-              <div className='follow_btn'>
-                <button>팔로우</button>
-              </div>
-            </div>
+            <FollowProfile />
 
-            <div className='follow_profile'>
-              <div className='title_img_radius'>
-                <img
-                  src='https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=Bj3WvVLmBYAAX_EHiGC&oh=d81e5bbacfadeb11cf773cc7c6a3650d&oe=5F004B70'
-                  alt='>WECODE'
-                />
-              </div>
-              <div className='follow_form'>
-                <a className='feed_title_main-head' href=''>
-                  wecode_bootcamp
-                </a>
-                <a className='feed_title_sub-head sidebar_name' href=''>
-                  회원님을 팔로우합니다
-                </a>
-              </div>
-              <div className='follow_btn'>
-                <button>팔로우</button>
-              </div>
-            </div>
+            <FollowProfile />
 
-            <div className='follow_profile'>
-              <div className='title_img_radius'>
-                <img
-                  src='https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=Bj3WvVLmBYAAX_EHiGC&oh=d81e5bbacfadeb11cf773cc7c6a3650d&oe=5F004B70'
-                  alt='>WECODE'
-                />
-              </div>
-              <div className='follow_form'>
-                <a className='feed_title_main-head' href=''>
-                  wecode_bootcamp
-                </a>
-                <a className='feed_title_sub-head sidebar_name' href=''>
-                  회원님을 팔로우합니다
-                </a>
-              </div>
-              <div className='follow_btn'>
-                <button>팔로우</button>
-              </div>
-            </div>
+            <FollowProfile />
 
-            <div className='follow_profile'>
-              <div className='title_img_radius'>
-                <img
-                  src='https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=Bj3WvVLmBYAAX_EHiGC&oh=d81e5bbacfadeb11cf773cc7c6a3650d&oe=5F004B70'
-                  alt='>WECODE'
-                />
-              </div>
-              <div className='follow_form'>
-                <a className='feed_title_main-head' href=''>
-                  wecode_bootcamp
-                </a>
-                <a className='feed_title_sub-head sidebar_name' href=''>
-                  wecode님 외 6명이 팔로우합니다
-                </a>
-              </div>
-              <div className='follow_btn'>
-                <button>팔로우</button>
-              </div>
-            </div>
+            <FollowProfile />
 
-            <div className='follow_profile'>
-              <div className='title_img_radius'>
-                <img
-                  src='https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=Bj3WvVLmBYAAX_EHiGC&oh=d81e5bbacfadeb11cf773cc7c6a3650d&oe=5F004B70'
-                  alt='>WECODE'
-                />
-              </div>
-              <div className='follow_form'>
-                <a className='feed_title_main-head' href=''>
-                  wecode_bootcamp
-                </a>
-                <a className='feed_title_sub-head sidebar_name' href=''>
-                  회원님을 팔로우합니다
-                </a>
-              </div>
-              <div className='follow_btn'>
-                <button>팔로우</button>
-              </div>
-            </div>
+            <FollowProfile />
           </div>
 
           <div className='footer_info'>
