865e150d69805d2234ef4edc60fa8adcb2f79f37
solmii <rkdthfal4759@naver.com>
2020-06-18 23:49:35 +0900

    [FIX] .then 함수 누락으로 작동 안된 문제 해결

--- src/Components/Uploadcomment.js
+++ src/Components/Uploadcomment.js
@@ -11,7 +11,6 @@ class Uploadcomment extends React.Component {
     this.state = {
       userInput: '',
       commentArr: [],
-      commentCount: 2,
     };
   }
   // 사용자가 입력한 comment input값으로 setState 해주는 이벤트
@@ -20,21 +19,33 @@ class Uploadcomment extends React.Component {
       userInput: e.target.value,
     });
   };
-  // 게시 버튼 클릭했을 때 댓글창 추가 이벤트
+  // 게시 data get 이벤트
+  handleData = () => {
+    fetch('http://10.58.5.169:8000/comment/posting')
+      .then((res) => res.json())
+      .then((res) => this.setState({ commentArr: res.message }));
+  };
+  // mount 직후에 서버에서 댓글 가져오기 - 한번만 실행
+  componentDidMount() {
+    this.handleData();
+  }
+  // 게시 버튼 클릭했을 때 댓글창 추가 이벤트 + 백앤드 서버에도 전솔
   addFormHandler = () => {
-    // 댓글 백앤드 서버로 POST (보내주기)
-    fetch('http://10.58.7.42:8000/comment/', {
+    // 댓글 POST
+    fetch('http://10.58.5.169:8000/comment/posting', {
       method: 'post',
       headers: {
-        Authorization: localStorage.getItem('access_token'), // access_token을 백앤드한테 보내줌
+        Authorization: localStorage.getItem('access_token'), // access_token을 Authorization로 백앤드한테 보내줌
       },
       body: JSON.stringify({
-        Comments: this.state.userInput, // userInput을 백앤드한테 보내줌
+        comments: this.state.userInput, // userInput을 comment로 백앤드한테 보내줌
       }),
+    }).then(() => {
+      // 댓글을 새로 쓸때마다, 서버에 댓글 post 하고 get 해옴!
+      if (this.state.userInput) {
+        this.setState({ userInput: '' }, () => this.handleData()); // 콜백 함수. 함수의 인자로 들어간 함수.
+      }
     });
-    if (this.state.userInput) {
-      this.setState({ commentArr: this.state.commentArr.concat(this.state.userInput), userInput: '', commentCount: this.state.commentCount + 1 });
-    }
   };
   // Enter 눌렀을 때 댓글창 추가 이벤트
   pressEnterHandler = (e) => {
@@ -44,18 +55,16 @@ class Uploadcomment extends React.Component {
   };
   // 댓글 클릭시 댓글 삭제 이벤트
   commentDelHandler = (index) => {
-    console.log(index);
-
     this.setState({
       commentArr: this.state.commentArr.filter((_, idx) => idx !== index),
-      commentCount: this.state.commentCount - 1,
     });
   };
   render() {
+    console.log(this.state.commentArr);
     return (
       <>
         <div className='more_comment'>
-          <a href=''>댓글 {this.state.commentCount}개 모두 보기</a>
+          <a href=''>댓글 {this.state.commentArr.length}개 모두 보기</a>
         </div>
 
         <div className='comments'>
